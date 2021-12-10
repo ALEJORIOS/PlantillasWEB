3ccb9157f8f3dd340707ee6ac60289aef54cb5a8
sudoAlejandro <luisrios.lar@gmail.com>
2021-12-10 02:52:26 -0500

    landscape responsive design
    Empresarial template with landscape responsive design.

--- Empresarial/src/public/js/scripts.js
+++ Empresarial/src/public/js/scripts.js
@@ -1,28 +1,36 @@
-seeInicio();
-
 function seeInicio() {
     disableAll();
     document.getElementById('startC').style.display = "block";
+    showNav();
+    focus();
 }
 
 function seeServicios() {
     disableAll();
     document.getElementById('services').style.display = "block";
+    showNav();
+    focus();
 }
 
 function seeEstrategias() {
     disableAll();
     document.getElementById('strategies').style.display = "block";
+    showNav();
+    focus();
 }
 
 function seeClientes() {
     disableAll();
     document.getElementById('costumers').style.display = "block";
+    showNav();
+    focus();
 }
 
 function seeContactenos() {
     disableAll();
     document.getElementById('contactus').style.display = "block";
+    showNav();
+    focus();
 }
 
 function disableAll() {
@@ -34,11 +42,23 @@ function disableAll() {
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
+    seeInicio();
+    showNav();
+
+    // Loader
     setTimeout(function() {
         var body = document.getElementsByTagName("BODY")[0];
         body.style.visibility = 'visible';
         document.getElementById('loader').style.display = 'none';
-
-
     }, 500);
 });
+
+function showNav() {
+    document.getElementById('navigation').classList.toggle('show');
+    document.body.classList.toggle('scrolls');
+}
+
+function focus() {
+    const element = document.querySelector("#content");
+    element.scrollIntoView();
+}
